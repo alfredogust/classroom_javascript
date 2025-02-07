abb6330c3895555a84d7080b3dc6cb7e650dd1fb
alfredogust <alfredog.fu@gmail.com>
2025-02-07 18:28:51 -0300

    refactor: centralize getIdealWeightByGender() function call in index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // Importing functions from the exercises file
 const { helloWorld, informedNumber, sumTwoNumbers, gradeAverage, convertMetersToCentimeters,
     circleRadius, squareAreaAndDouble, calculateSalary, fahrenheitToCelsius, celsiusToFahrenheit,
-    calculateThreeNumbers, calculateIdealWeight } = require("./exercises/basic-operations/basic-operations");
+    calculateThreeNumbers, calculateIdealWeight, getIdealWeightByGender } = require("./exercises/basic-operations/basic-operations");
 const runExercises = () => {
     console.log("Exercises in execution now:\n");
 
@@ -18,7 +18,8 @@ const runExercises = () => {
     //fahrenheitToCelsius();
     //celsiusToFahrenheit();
     //calculateThreeNumbers();
-    calculateIdealWeight();
+    //calculateIdealWeight();
+    getIdealWeightByGender();
 };
 
 runExercises();
